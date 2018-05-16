00a00c37158642d7fa0efc2c52fe516f382644a6
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-05-16 21:23:33 +0100

    Implement helper for deferring field writes
    Intended for filling in chunk lengths once they're known. Only works
    with seekable streams.

---
+++ DataTools.SqlBulkData.UnitTests/Serialisation/ReservedFieldTests.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using DataTools.SqlBulkData.Serialisation;
+using NUnit.Framework;
+
+namespace DataTools.SqlBulkData.UnitTests.Serialisation
+{
+    [TestFixture]
+    public class ReservedFieldTests
+    {
+        [Test]
+        public void ReservesSpaceForField()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteByte(stream, 0x01);
+            new ReservedField<byte>(stream, Serialiser.WriteByte);
+            Serialiser.WriteByte(stream, 0x03);
+
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x01, 0x00, 0x03 }));
+        }
+
+        [Test]
+        public void WritesValueIntoReservedSpace()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteByte(stream, 0x01);
+            var field = new ReservedField<byte>(stream, Serialiser.WriteByte);
+            Serialiser.WriteByte(stream, 0x03);
+            field.Write(0x02);
+
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x01, 0x02, 0x03 }));
+        }
+
+        [Test]
+        public void RestoresPositionAfterWritingValue()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteByte(stream, 0x01);
+            var field = new ReservedField<byte>(stream, Serialiser.WriteByte);
+            Serialiser.WriteByte(stream, 0x03);
+
+            Assume.That(stream.Position, Is.EqualTo(3));
+
+            field.Write(0x02);
+
+            Assert.That(stream.Position, Is.EqualTo(3));
+        }
+    }
+}
---
+++ DataTools.SqlBulkData/Serialisation/ReservedField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DataTools.SqlBulkData.Serialisation
+{
+    public struct ReservedField<T> where T : struct
+    {
+        private readonly Stream stream;
+        private readonly Action<Stream, T> write;
+        private readonly long mark;
+
+        public ReservedField(Stream stream, Action<Stream, T> write)
+        {
+            if (!stream.CanWrite) throw new ArgumentException("Stream is not writable.", nameof(stream));
+            if (!stream.CanSeek) throw new ArgumentException("Stream is not seekable.", nameof(stream));
+            this.stream = stream;
+            this.write = write;
+            mark = stream.Position;
+            write(stream, default(T));
+        }
+
+        public void Write(T value)
+        {
+            var currentPosition = stream.Position;
+            stream.Seek(mark, SeekOrigin.Begin);
+            write(stream, value);
+            stream.Seek(currentPosition, SeekOrigin.Begin);
+        }
+    }
+}
