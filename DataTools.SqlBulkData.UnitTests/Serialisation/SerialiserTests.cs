24c00ea9a99a59b4cdc4e401b917e7a88779a27f
Alex Davidson <alex.davidson@bluewire-technologies.com>
2018-05-16 17:25:16 +0100

    Add serialisation tools with tests
    Primitives used to compose chunks.

---
+++ DataTools.SqlBulkData.UnitTests/Serialisation/SerialiserTests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using DataTools.SqlBulkData.Serialisation;
+using NUnit.Framework;
+
+namespace DataTools.SqlBulkData.UnitTests.Serialisation
+{
+    [TestFixture]
+    public class SerialiserTests
+    {
+        [Test]
+        public void RoundTripsGuid()
+        {
+            var stream = new MemoryStream();
+            var guid = new Guid("{00112233-4455-6677-8899-AABBCCDDEEFF}");
+            Serialiser.WriteGuid(stream, guid);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadGuid(stream), Is.EqualTo(guid));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] {
+                0x33, 0x22, 0x11, 0x00,
+                0x55, 0x44,
+                0x77, 0x66, 
+                0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF
+            }));
+        }
+
+        [Test]
+        public void RoundTripsSinglePrecision()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteSingle(stream, (float)Math.E);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadSingle(stream), Is.EqualTo((float)Math.E));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x54, 0xF8, 0x2D, 0x40 }));
+        }
+
+        [Test]
+        public void RoundTripsDoublePrecision()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteDouble(stream, Math.PI);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadDouble(stream), Is.EqualTo(Math.PI));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x18, 0x2D, 0x44, 0x54, 0xFB, 0x21, 0x09, 0x40 }));
+        }
+
+        [Test]
+        public void RoundTripsInt64()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteInt64(stream, -7757820597154693433);  // 0x9456AF450362BEC7
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadInt64(stream), Is.EqualTo(-7757820597154693433));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0xC7, 0xBE, 0x62, 0x03, 0x45, 0xAF, 0x56, 0x94 }));
+        }
+
+        [Test]
+        public void RoundTripsUInt64()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteUInt64(stream, 0x9456AF450362BEC7);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadUInt64(stream), Is.EqualTo(0x9456AF450362BEC7));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0xC7, 0xBE, 0x62, 0x03, 0x45, 0xAF, 0x56, 0x94 }));
+        }
+
+        [Test]
+        public void RoundTripsInt32()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteInt32(stream, -1806258363);  // 0x9456AF45
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadInt32(stream), Is.EqualTo(-1806258363));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x45, 0xAF, 0x56, 0x94 }));
+        }
+
+        [Test]
+        public void RoundTripsUInt32()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteUInt32(stream, 0x9456AF45);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadUInt32(stream), Is.EqualTo(0x9456AF45));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x45, 0xAF, 0x56, 0x94 }));
+        }
+
+        [Test]
+        public void RoundTripsInt16()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteInt16(stream, -27562);  // 0x9456
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadInt16(stream), Is.EqualTo(-27562));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x56, 0x94 }));
+        }
+
+        [Test]
+        public void RoundTripsUInt16()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteUInt16(stream, 0x9456);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadUInt16(stream), Is.EqualTo(0x9456));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x56, 0x94 }));
+        }
+
+        [Test]
+        public void RoundTripsVariableLengthString()
+        {
+            var stream = new MemoryStream();
+            Serialiser.WriteString(stream, "Hello");
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadString(stream), Is.EqualTo("Hello"));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x05, 0x00, 0x00, 0x00, 0x48, 0x65, 0x6C, 0x6C, 0x6F }));
+        }
+
+        [Test]
+        public void RoundTripsFixedLengthBytes()
+        {
+            var stream = new MemoryStream();
+            var bytes = new byte[] { 0xC7, 0xBE, 0x62, 0x03, 0x45, 0xAF };
+            Serialiser.WriteFixedLengthBytes(stream, bytes);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadFixedLengthBytes(stream, new byte[6]), Is.EqualTo(bytes));
+            Assert.That(stream.ToArray(), Is.EqualTo(bytes));
+        }
+
+        [Test]
+        public void RoundTripsVariableLengthBytes()
+        {
+            var stream = new MemoryStream();
+            var bytes = new byte[] { 0xC7, 0xBE, 0x62, 0x03, 0x45, 0xAF };
+            Serialiser.WriteBytes(stream, bytes);
+            stream.Position = 0;
+
+            Assert.That(Serialiser.ReadBytes(stream), Is.EqualTo(bytes));
+            Assert.That(stream.ToArray(), Is.EqualTo(new byte[] { 0x06, 0x00, 0x00, 0x00 }.Concat(bytes)));
+        }
+
+        [Test]
+        public void AlignReadDoesNotModifyAlreadyAlignedPosition()
+        {
+            var stream = new MemoryStream(new byte[8]);
+            stream.Position = 4;
+
+            Assert.That(Serialiser.TryAlignRead(stream, 4), Is.True);
+            Assert.That(stream.Position, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void AlignReadAdvancesToAlignedPosition()
+        {
+            var stream = new MemoryStream(new byte[8]);
+            stream.Position = 5;
+
+            Assert.That(Serialiser.TryAlignRead(stream, 4), Is.True);
+            Assert.That(stream.Position, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void AlignReadReturnsFalseIfEndOfReadOnlyStreamWouldBePassed()
+        {
+            var stream = new MemoryStream(new byte[5], false);
+            stream.Position = 5;
+
+            Assert.That(Serialiser.TryAlignRead(stream, 4), Is.False);
+            Assert.That(stream.Position, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void AlignWriteDoesNotModifyAlreadyAlignedPosition()
+        {
+            var stream = new MemoryStream(new byte[8]);
+            stream.Position = 4;
+
+            Serialiser.AlignWrite(stream, 4);
+            Assert.That(stream.Position, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void AlignWriteAdvancesToAlignedPosition()
+        {
+            var stream = new MemoryStream(new byte[8]);
+            stream.Position = 5;
+
+            Serialiser.AlignWrite(stream, 4);
+            Assert.That(stream.Position, Is.EqualTo(8));
+        }
+
+        [Test]
+        public void AlignWriteExtendsStreamIfEndOfExpandableStreamWouldBePassed()
+        {
+            var stream = new MemoryStream();
+            stream.Position = 5;
+
+            Serialiser.AlignWrite(stream, 4);
+            Assert.That(stream.Position, Is.EqualTo(8));
+            Assert.That(stream.Length, Is.EqualTo(8));
+        }
+    }
+}
---
+++ DataTools.SqlBulkData/Serialisation/Serialiser.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace DataTools.SqlBulkData.Serialisation
+{
+    /// <summary>
+    /// Serialises primitive types in accordance with the rules in README.md. Asserts against alignment by default in DEBUG builds.
+    /// </summary>
+    public static class Serialiser
+    {
+        private const int BufferSize = 16;
+        private const int MaximumAlignment = 16;
+        private static readonly ThreadLocal<byte[]> sharedBuffer = new ThreadLocal<byte[]>(() => new byte[BufferSize]);
+        private static readonly byte[] readAlignBuffer = new byte[MaximumAlignment];
+        private static readonly byte[] writeAlignBuffer = new byte[MaximumAlignment];
+
+        public static bool IsAligned(Stream s, int alignment) => s.Position % alignment == 0;
+        
+        /// <summary>
+        /// Seek forwards as necessary to align the next read as specified.
+        /// Throws EndOfStreamException if the seek would go beyond the end of the stream.
+        /// </summary>
+        public static void AlignRead(Stream stream, int alignment)
+        {
+            if (TryAlignRead(stream, alignment)) return;
+            throw new EndOfStreamException();
+        }
+
+        /// <summary>
+        /// Seek forwards as necessary to align the next read as specified.
+        /// </summary>
+        /// <returns>False if the seek would go beyond the end of the stream.</returns>
+        public static bool TryAlignRead(Stream stream, int alignment)
+        {
+            if (!stream.CanRead) throw new NotSupportedException("Stream is not readable.");
+            var adjustment = GetAlignmentAdjustment(stream, alignment);
+            if (adjustment == 0) return true;
+            if (stream.Position + adjustment > stream.Length) return false;
+
+            Debug.Assert(adjustment <= MaximumAlignment);
+            var count = stream.Read(readAlignBuffer, 0, adjustment);
+            Debug.Assert(count == adjustment);
+            return true;
+        }
+
+        /// <summary>
+        /// Write empty bytes as necessary to align the next write as specified.
+        /// </summary>
+        public static void AlignWrite(Stream stream, int alignment)
+        {
+            if (!stream.CanWrite) throw new NotSupportedException("Stream is not readable.");
+            var adjustment = GetAlignmentAdjustment(stream, alignment);
+            if (adjustment == 0) return;
+
+            Debug.Assert(adjustment <= MaximumAlignment);
+            stream.Write(writeAlignBuffer, 0, adjustment);
+        }
+
+        public static bool IsEndOfStream(Stream stream) => stream.Position >= stream.Length;
+
+        public static Guid ReadGuid(Stream stream) => new Guid(ReadFixedLengthBytesInternal(stream, 16, 8));
+        public static void WriteGuid(Stream stream, Guid value) => WriteFixedLengthBytesInternal(stream, value.ToByteArray(), 8);
+
+        public static float ReadSingle(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 4), BitConverter.ToSingle);
+        public static void WriteSingle(Stream stream, float value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes));
+        public static double ReadDouble(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 8, 4), BitConverter.ToDouble);
+        public static void WriteDouble(Stream stream, double value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes), 4);
+
+        public static long ReadInt64(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 8, 4), BitConverter.ToInt64);
+        public static void WriteInt64(Stream stream, long value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes), 4);
+        public static ulong ReadUInt64(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 8, 4), BitConverter.ToUInt64);
+        public static void WriteUInt64(Stream stream, ulong value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes), 4);
+
+        public static int ReadInt32(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 4), BitConverter.ToInt32);
+        public static void WriteInt32(Stream stream, int value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes));
+        public static uint ReadUInt32(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 4),  BitConverter.ToUInt32);
+        public static void WriteUInt32(Stream stream, uint value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes));
+
+        public static short ReadInt16(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 2), BitConverter.ToInt16);
+        public static void WriteInt16(Stream stream, short value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes));
+        public static ushort ReadUInt16(Stream stream) => FromBytes(ReadFixedLengthBytesInternal(stream, 2), BitConverter.ToUInt16);
+        public static void WriteUInt16(Stream stream, ushort value) => WriteFixedLengthBytesInternal(stream, ToBytes(value, BitConverter.GetBytes));
+
+        public static byte ReadByte(Stream stream)
+        {
+            var value = stream.ReadByte();
+            if (value < byte.MinValue || value > byte.MaxValue) throw new EndOfStreamException();
+            return (byte)value;
+        }
+        public static void WriteByte(Stream stream, byte value) => stream.WriteByte(value);
+
+        public static string ReadString(Stream stream)
+        {
+            var stringBuffer = ReadBytes(stream);
+            return Encoding.UTF8.GetString(stringBuffer);
+        }
+        public static void WriteString(Stream stream, string value)
+        {
+            var stringBuffer = Encoding.UTF8.GetBytes(value);
+            WriteBytes(stream, stringBuffer);
+        }
+
+        public static byte[] ReadFixedLengthBytes(Stream stream, byte[] buffer) => ReadFixedLengthBytesInternal(stream, buffer, 1);
+        public static void WriteFixedLengthBytes(Stream stream, byte[] value) => WriteFixedLengthBytesInternal(stream, value, 1);
+
+        public static byte[] ReadBytes(Stream stream) => ReadVariableLengthBuffer(stream);
+        public static void WriteBytes(Stream stream, byte[] value) => WriteVariableLengthBuffer(stream, value);
+
+        private static byte[] ReadFixedLengthBytesInternal(Stream stream, int count) => ReadFixedLengthBytesInternal(stream, count, count);
+        private static byte[] ReadFixedLengthBytesInternal(Stream stream, int count, int alignment)
+        {
+            Debug.Assert(count <= BufferSize);
+            CheckAlignment(stream, alignment);
+            var buffer = sharedBuffer.Value;
+            CheckedRead(stream, buffer, count);
+            return buffer;
+        }
+        private static byte[] ReadFixedLengthBytesInternal(Stream stream, byte[] buffer, int alignment)
+        {
+            Debug.Assert(buffer != sharedBuffer.Value);
+            CheckAlignment(stream, alignment);
+            CheckedRead(stream, buffer, buffer.Length);
+            return buffer;
+        }
+
+        private static void WriteFixedLengthBytesInternal(Stream stream, byte[] bytes) => WriteFixedLengthBytesInternal(stream, bytes, bytes.Length);
+        private static void WriteFixedLengthBytesInternal(Stream stream, byte[] bytes, int alignment)
+        {
+            CheckAlignment(stream, alignment);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        private static byte[] ReadVariableLengthBuffer(Stream stream)
+        {
+            var length = ReadInt32(stream);
+            var buffer = new byte[length];
+            CheckedRead(stream, buffer, buffer.Length);
+            return buffer;
+        }
+
+        private static void WriteVariableLengthBuffer(Stream stream, byte[] buffer)
+        {
+            WriteInt32(stream, buffer.Length);
+            stream.Write(buffer, 0, buffer.Length);
+        }
+
+        private static T FromBytes<T>(byte[] bytes, Func<byte[], int, T> convert)
+        {
+            if (!BitConverter.IsLittleEndian) Array.Reverse(bytes);
+            return convert(bytes, 0);
+        }
+
+        private static byte[] ToBytes<T>(T value, Func<T, byte[]> convert)
+        {
+            var bytes = convert(value);
+            if (!BitConverter.IsLittleEndian) Array.Reverse(bytes);
+            return bytes;
+        }
+
+        private static void CheckedRead(Stream stream, byte[] buffer, int count)
+        {
+            Debug.Assert(count <= buffer.Length);
+            var read = stream.Read(buffer, 0, count);
+            if (read < count) throw new EndOfStreamException();
+        }
+
+        private static int GetAlignmentAdjustment(Stream stream, int alignment)
+        {
+            if (alignment <= 0) throw new ArgumentException("Alignment must be positive.", nameof(alignment));
+            if (alignment > MaximumAlignment) throw new ArgumentException($"Alignment cannot exceed {MaximumAlignment}.", nameof(alignment));
+
+            var misalignment = stream.Position % alignment;
+            if (misalignment == 0) return 0;
+            var adjustment = alignment - misalignment;
+            Debug.Assert(adjustment <= MaximumAlignment);
+            return (int)adjustment;
+        }
+
+        public static bool EnableParanoidAlignmentChecks { get; set; } = false;
+
+        private static void CheckAlignment(Stream s, int alignment)
+        {
+            if (!EnableParanoidAlignmentChecks) return;
+            if (alignment <= 1) return; // No alignment check required. Avoid expensive call to Position.
+            if (!IsAligned(s, alignment)) throw new InvalidOperationException($"Stream position is not aligned with a {alignment}-byte boundary.");
+        }
+
+        static Serialiser()
+        {
+            EnableParanoidAlignmentChecksInDebugBuilds();
+        }
+
+        [Conditional("DEBUG")]
+        private static void EnableParanoidAlignmentChecksInDebugBuilds()
+        {
+            EnableParanoidAlignmentChecks = true;
+        }
+    }
+}
